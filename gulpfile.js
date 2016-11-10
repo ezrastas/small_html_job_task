73f72a26488b64eae7becbbff5746c96ecdfb80b
Ezra Stas Bbabushkin <diablostas@mail.ru>
2016-11-11 00:40:34 +0300

    gulp watch is redy for change a less-files

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var minifyCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+
+gulp.task('less', function () {
+  gulp.src('style/less/main.less')
+  .pipe(less())
+  .pipe(concat('main.css'))
+  .pipe(gulp.dest('style/'))
+  .pipe(minifyCSS(''))
+  .pipe(rename({suffix: ".min"}))
+  .pipe(gulp.dest('style/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('style/less/main.less', ['less'])
+});
